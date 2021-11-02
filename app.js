15c55d83bb671b6ecfee65a5d56f348e9d89a560
juliadud <yuliiadudkina@gmail.com>
2021-11-02 13:31:59 +0200

    add attacks logic

---
+++ app.js
@@ -0,0 +1,43 @@
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
+const app = Vue.createApp({
+    data(){
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0,
+        }
+    },
+    computed:{
+      monsterBarWidth(){
+          return {width: this.monsterHealth + '%'};
+      },
+      playerBarWidth(){
+         return {width: this.playerHealth + '%'};
+      },
+      mayUseSpecialAttack(){
+         return this.currentRound % 3 !== 0;
+      }
+    },
+    methods:{
+        attackMonster(){
+            this.currentRound++;
+            const attackValue =  getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackValue =  getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        },
+        specialAttack(){
+            this.currentRound++;
+            const attackValue =  getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        }
+    }
+})
+app.mount('#game');
